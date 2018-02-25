5912e1f8a2e3bc768f3a0b5225a4b7088bf3e7da
Jiangy82 <zimejohn@126.com>
2018-02-26 00:26:37 +0800

    属性文件修改

--- src/main/java/com/mmall/app/B2FController.java
+++ src/main/java/com/mmall/app/B2FController.java
@@ -1,12 +1,28 @@
 package com.mmall.app;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import javax.annotation.Resource;
 
-@Controller
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mmall.dao.UserMapper;
+import com.mmall.pojo.User;
+
+@RestController
 public class B2FController {
+	@Resource
+	private UserMapper userMapper;
+	
     @RequestMapping(value="/test")
 	public String testB2F() {
 		return "index";
 	}
+    @GetMapping("user")
+    public ResponseEntity<User> getUser(){
+        return ResponseEntity.ok(userMapper.selectByPrimaryKey(1));
+    }
 }
